4cc7f544da9825c897a2266bf53694a34c86aab1
danny <johndyates@gmail.com>
2022-06-21 08:40:20 -0500

    initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className=' text-center fixed-bottom'>
+      <a href="mailto:Johndyates@gmail.com"><img className="p-2" src="https://img.icons8.com/fluency/48/undefined/gmail-new.png" /></a>
+      <a href="https://www.github.com/cycoconutz"><img src="https://img.icons8.com/ios-filled/46/undefined/github.png" /></a>
+      <a href="https://www.linkedin.com/in/danny-yates-626571b5/"><img src="https://img.icons8.com/color/48/undefined/linkedin.png" /></a>
+      <br></br>
+      <p className="font-weight-bold">&copy; Danny Yates 2022</p>
+    </footer>
+  );
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>Github API Interface</header>
+    )
+  }
+}
+
+export default Header
